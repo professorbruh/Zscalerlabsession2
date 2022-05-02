fbbd5d8c9f8702585d35a0b3bf3a5010ed050ce5
Anil-Ranolia <anilranolia5442003@gmail.com>
2022-05-02 09:23:40 +0530

    Some test method added.... config needed

---
+++ src/test/java/com/zscalerlabsession/zscalerlabsession/AccountControllerTests.java
@@ -0,0 +1,38 @@
+package com.zscalerlabsession.zscalerlabsession;
+
+import com.zscalerlabsession.zscalerlabsession.Model.Account;
+import com.zscalerlabsession.zscalerlabsession.Repository.AccountRepository;
+import com.zscalerlabsession.zscalerlabsession.controllers.AccountController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AccountControllerTests{
+
+    @Autowired
+    AccountController accountController;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    void testDbConncetion(){
+        assertTrue(accountRepository.count() > 0);
+    }
+
+    @Test
+    void createAccountTest(){
+        long num = accountRepository.count();
+        Account account = new Account(213, 2334432, 2354.10, "salary");
+        accountController.createCAccount(account);
+
+        assertTrue(accountRepository.count() == num + 1);
+    }
+
+    @Test
+    void getDetailsTest(){
+        Account account = accountRepository.fetchAccountById(1234);
+        assertNull(account);
+    }
+}
--- src/test/java/com/zscalerlabsession/zscalerlabsession/ZscalerlabsessionApplicationTests.java
+++ src/test/java/com/zscalerlabsession/zscalerlabsession/ZscalerlabsessionApplicationTests.java
@@ -8,6 +8,7 @@ class ZscalerlabsessionApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
